d22f30b7edc4b08c3c7fba3ccc2506f65d81a254
nkgotcode <ngunhucutay@gmail.com>
2021-07-04 16:16:20 -0500

    Initial Commit

---
+++ PhoneEatsFirst/PhoneEatsFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneEatsFirstApp.swift
+//  PhoneEatsFirst
+//
+//  Created by itsnk on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PhoneEatsFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
